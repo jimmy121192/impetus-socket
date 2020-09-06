f138c3d0d262147c7d253d1e37c115901ffb0e7f
Jimmy Truong <hoangbao1992@gmail.com>
2020-09-06 08:07:36 -0700

    first commit

---
+++ socket.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8880;
+
+const server = http.createServer(app);
+
+
+var io = require("socket.io")(server, {
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
+});
+
+    var chatSpace = io.of("/chat")
+    chatSpace.on("connection", (socket)=>{
+
+    socket.on("join_room", (data)=>{
+        socket.join(data.roomName);
+    })
+    console.log(socket.id+ " is connected");
+    socket.on("send_msg", (data)=>{
+        chatSpace.to(data.roomName).emit("incoming_msg", data)
+    })
+})
+server.listen(port);
