86df033a92a40329800aac33d52b6d3d67017306
Jimmy Truong <hoangbao1992@gmail.com>
2020-09-06 13:10:35 -0700

    update

--- socket.js
+++ socket.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 8880;
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://impetus.netlify.app");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
     if (req.method === 'OPTIONS') {
