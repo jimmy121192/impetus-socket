8198006ab462167b36933cd872d96005e0305b4e
Jimmy Truong <hoangbao1992@gmail.com>
2020-09-06 08:21:51 -0700

    first commit

--- socket.js
+++ socket.js
@@ -8,8 +8,8 @@ const server = http.createServer(app);
 var io = require("socket.io")(server, {
     handlePreflightRequest: (req, res) => {
         const headers = {
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Headers": "X-Requested-With, Content-Type, Authorization",
+            "Access-Control-Allow-Origin": "https://impetus.netlify.app", //or the specific origin you want to give access to,
             "Access-Control-Allow-Credentials": true
         };
         res.writeHead(200, headers);
